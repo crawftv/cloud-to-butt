100c25b7a902169b4cabfd8584b4d750b3101da3
crawdogginit <crawford.collins@gmail.com>
2017-01-18 21:59:07 -0600

    updated files for a test

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,24 +1,24 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
-	
+
 	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
 	    || node.classList.indexOf('ace_editor') > -1) {
 		return;
 	}
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -32,16 +32,14 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bBruce Chassy\b/g, "Bruce Chassy, a University of Illinois who has recieved funding from Monsanto, ");
+	v = v.replace(/\bKevin Folta\b/g, "Kevin Folta, a University of Illinois who has recieved funding from Monsanto, ");
+	v = v.replace(/\bTaking on the Food Industry, One Blog Post at a Time\b/g, "Taking on the Food Industry, One Blog Post at a Time. This article references the works of professors who have been funded by Monsanto.");
+	v = v.replace(/\bFoes of Modified Corn Find Support in a Study\b/g, "Foes of Modified Corn Find Support in a Study. This article references the works of professors who have been funded by Monsanto.");
+
 	textNode.nodeValue = v;
 }
-
-
